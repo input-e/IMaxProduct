56a54cf410733fbc2ed6b92c6091fbb7fb8bc1e4
input-e <nikitazhartun98@gmail.com>
2022-05-29 01:20:54 +0300

    'adding_the_main_parse_function'

--- Day_object.py
+++ Day_object.py
@@ -1,40 +1,60 @@
+from pickletools import int4
 from typing import Dict, List
 from bs4 import BeautifulSoup
 # from bs4 import SoupStrainer 
 
 
 class Day():
-    def __init__(self, group, table):
+    def __init__(self, group: int, table: BeautifulSoup.Tag):
         self.group = group
-        self.table = table
-        self.day_date = self.day_date_parse(self.table)
+        self.day_date = self.day_date_parse(table)
+        self.subject = self.subject_parse(table)
+        
 
-    def day_date_parse(self, table) -> Dict:
-        iteration = 0
+    def day_date_parse(self, table: BeautifulSoup.Tag) -> Dict:
         contain = {'day':'', 'date':''}
-        while True:
-            if iteration == len(table.find_all('tr')):
-                raise Exception('No such number of group!')
-
+        for iteration in range(0, len(table.find_all('tr'))-1):
             if len(table.find_all('tr')[iteration].find_all('td')):
                 terminator_1 = table.find_all('tr')[0].find_all('td')[0].text.find('-')
                 terminator_2 = table.find_all('tr')[0].find_all('td')[0].text.find(',')
                 contain['day'] = table.find_all('tr')[0].find_all('td')[0].text[terminator_1+2:terminator_2]
                 contain['date'] = table.find_all('tr')[0].find_all('td')[0].text[terminator_2+2:].rstrip()
                 return contain
+        
+        if contain['day'] == '' or contain['date'] == '':
+            raise Exception('There is no information about day and data')
 
-            iteration += 1
     
-    def day_show(self):
-        pass
+    def subject_parse(self, table: BeautifulSoup.Tag) ->list:
 
-    def part_show(self, number, details="all"):
-        pass
+        def eject_data(row: int, column: int, table: BeautifulSoup.Tag) ->list:
+            count = 0
+            while table.find_all('tr')[row+2].find('td').text.strip() == str(count+1):
+                pass
+        
 
+
+         # search for the position of group number in row
+        def find_position(table: BeautifulSoup.Tag) ->int:
+            for r in range(0, len(table.find_all('tr'))-1):
+                if table.find_all('tr')[r].find('td').text.strip() == '№':
+                    for col in range(1, len(table.find_all('tr')[r].find_all('td'))):
+                        if table.find_all('tr')[r].find_all('td')[col].text.strip() == str(self.group):
+                            return r, col
+
+            raise Exception('No such grop number here')
+
+        
+        row, column = find_position(table)
+        return eject_data(row, column, table)
+
+        
     def show(self):
         pass
 
 
-class Pare():
-    def __init__(self, sell_position):
-        pass
+    class Pare():
+        def __init__(self, number, main_content, classroom_number):
+            self.number = number
+            self.main_content = main_content
+            self.classroom_number = classroom_number
